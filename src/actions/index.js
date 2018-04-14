cfb198fc38827eeb2af8d398b36ceeaeecd75ce0
Nitin <nitin@smove.sg>
2018-04-14 13:00:35 +0800

    Bind fetchWeather action to SearchBar

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 import axios from 'axios';
 
-export function fetchWeather(city) {
+export default function fetchWeather(city) {
   const URL = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(URL);
 
--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import fetchWeather from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
   constructor(props) {
     super(props);
@@ -18,6 +21,14 @@ export default class SearchBar extends Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
+
+    // fetch data from API
+    this.props.fetchWeather(this.state.term);
+
+    // clear input
+    this.setState({
+      term: ''
+    });
   }
 
   render() {
@@ -36,3 +47,9 @@ export default class SearchBar extends Component {
     );
   }
 };
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
