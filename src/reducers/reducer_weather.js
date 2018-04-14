377c091a6dd78dd79c443fe63ca2ddb52f619290
Nitin <nitin@smove.sg>
2018-04-14 21:54:38 +0800

    Add WeatherList component

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from "../actions";
+
+export default function(state = [], action) {
   console.log('action', action);
+  switch(action.type) {
+    case FETCH_WEATHER: {
+      console.log(action.payload.data)
+      return [action.payload.data, ...state]
+    }
+  }
   return state;
 }
