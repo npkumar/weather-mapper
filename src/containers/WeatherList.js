ac0cfa4d032ad30e1da12a9c796042f33cd3e325
Nitin <nitin@smove.sg>
2018-04-14 22:44:11 +0800

    Add chart component

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/Chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={cityData.city.id}>
         <td>{ cityData.city.name } </td>
+        <td>
+          <Chart color='red' data={temps} />
+        </td>
+        <td>
+          <Chart color='green' data={pressures} />
+        </td>
+        <td>
+          <Chart color='blue' data={humidity} />
+        </td>
       </tr>
     )
   }
@@ -19,7 +33,6 @@ class WeatherList extends Component {
             <th>Temperature</th>
             <th>Pressure</th>
             <th>Humidity</th>
-            <th>{ this.props.weather.length }</th>
           </tr>
         </thead>
         <tbody>
